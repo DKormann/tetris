1bf7f9e3eae0c0d6a9e0b464e4f772d3a6ebe5d5
DKormann <dominikormann@outlook.com>
2025-03-26 10:22:09 +0100

    works

--- src/main.ts
+++ src/main.ts
@@ -24,22 +24,13 @@ for (let i = 1; i < x; i += 1)line(i*s, 0, i*s, y*s)
 for (let i = 1; i < y; i += 1)line(0, i*s, x*s, i*s)
 
 let pieces = [
-  [
-    [1, 1, 1, 1],
-    [0, 0, 0, 0],
-  ],
-  [
-    [1, 1, 0, 0],
-    [1, 1, 0, 0],
-  ],
-  [
-    [1, 1, 0, 0],
-    [0, 1, 1, 0],
-  ],
-  [
-    [0, 1, 1, 0],
-    [1, 1, 0, 0]
-  ],
+  [[0, 0, 0, 0],[1, 1, 1, 1],],
+  [[1, 1, 0, 0],[1, 1, 0, 0],],
+  [[1, 1, 0, 0],[0, 1, 1, 0],],
+  [[1, 1, 1, 0],[0, 0, 1, 0],],
+  [[0, 1, 0, 0],[1, 1, 1, 0],],
+  [[1, 0, 0, 0],[1, 1, 1, 0],],
+  [[0, 1, 1, 0],[1, 1, 0, 0]],
 ]
 
 pieces = pieces.map(p=>[...p, [0,0,0,0],[0,0,0,0]])
@@ -52,43 +43,66 @@ function getPieceTile(){
   return piece.map((row, i) => row.map((cell, j) => cell ? [{x: pos.x + j, y: pos.y + i}] : [])).flat(2)
 }
 
-function draw(color:string, tiles: Pos[]){
-  tiles.map(tile => {
-    context.fillStyle = color
-    context.fillRect(tile.x * s+1, tile.y * s+1, s - 2, s - 2)
-  })
 
-}
+const world = Array.from({length: y}, () => Array.from({length: x}, () => 0))
 
-function check(tiles: Pos[]){
-  return tiles.every(tile =>
-    tile.y < y && tile.x >= 0 && tile.x < x && tile.y >= 0
+function draw(tiles: Pos[]){tiles.map(tile=>world[tile.y][tile.x] = 1)}
+function clear(tiles: Pos[]){tiles.map(tile=>world[tile.y][tile.x] = 0)}
+function check(tiles: Pos[]){return tiles.every(tile =>
+  tile.y < y && tile.x >= 0 && tile.x < x && tile.y >= 0 && !world[tile.y][tile.x]
+)}
+
+function view(){
+  const pct = getPieceTile()
+  world.map((row, y) => 
+    row.map((cell, x) => {
+      context.fillStyle = cell ? "blue" :
+      !pct.every(tile => tile.x !== x || tile.y !== y) ? "red" : "white"
+      context.fillRect(x * s + 1, y * s + 1, s-2, s-2)
+
+    })
   )
 }
 
 function update(){
-  move(0, 1)
+  if (!move(0, 1)){
+    draw(getPieceTile())
+    pos = {x: 0, y: 0}
+    piece = pieces[Math.floor(Math.random() * pieces.length)]
+    console.log(piece);
+    
+  }
+  
+  for (let y = 0; y < world.length; y += 1){
+    if (world[y].every(cell => cell === 1)){
+      world.splice(y, 1)
+      world.unshift(Array.from({length: x}, () => 0))
+    }
+  }
+  view()
 }
 
 function move(x:number, y:number){
-  draw("white", getPieceTile())
+  clear(getPieceTile())
   pos.y += y
   pos.x += x
-  if (!check(getPieceTile())) {
+  let res = check(getPieceTile())
+  if (!res) {
     pos.y -= y
     pos.x -= x
   }
-  draw("blue", getPieceTile())
+  // draw(getPieceTile())
+  return res
 }
 
-setInterval(update, 1000)
+setInterval(update, 400)
 
 function rotate(){
   const prev = piece
-  draw("white", getPieceTile())
+  clear(getPieceTile())
   piece = piece[0].map((_, i) => piece.map(row => row[i])).reverse()
   if (!check(getPieceTile())) piece = prev
-  draw("blue", getPieceTile())
+  // draw(getPieceTile())
 }
 
 document.addEventListener("keydown", (e) => {
@@ -96,5 +110,7 @@ document.addEventListener("keydown", (e) => {
   if (e.key === "ArrowLeft") move(-1, 0)
   if (e.key === "ArrowDown") move(0, 1)
   if (e.key === " " || e.key === "ArrowUp") rotate()
+  view()
+
 })
 
